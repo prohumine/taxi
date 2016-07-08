9431e28825b73cf4682abbdda1a254c72377f004
Marvin Santizo <marvin@Marvins-Air.lan>
2016-07-07 19:37:09 -0600

    Added routes file and required it in app js file

--- app.js
+++ app.js
@@ -1,16 +1,25 @@
 var restify = require( 'restify' );
+var restifyValidation = require( 'restify-validation' );
 var config = require( './config/config.json' );
 var db = require( './models' );
 var server = restify.createServer( {
 	name: 'Taxi'
 } );
 
-server.use( restify.acceptParser( server.acceptable) );
+server.use( restify.acceptParser( server.acceptable ) );
 server.use( restify.queryParser() );
 server.use( restify.bodyParser() );
+server.use( restify.authorizationParser() );
+server.use( restify.CORS() );
+server.use( restify.fullResponse() );
+server.use( restifyValidation() );
+
+restify.CORS.ALLOW_HEADERS.push( 'authorization' );
 
 db.sequelize.sync( { force: true } ).then( function(){
 	server.listen( config.api.port, function(){
 		console.log( "Server listening on port " + config.api.port );
 	} );
 } );
+
+require( './config/routes' )( server );
---
+++ config/routes.js
@@ -0,0 +1,19 @@
+module.exports = function( server ){
+	var cont = require( '../controllers' );
+
+	server.get( '/drivers', cont.drivers.index );
+	server.post( '/drivers', cont.drivers.create );
+	server.get( '/drivers/:driver_id', cont.drivers.view );
+	server.put( '/drivers/:driver_id', cont.drivers.update );
+	server.del( '/drivers/:driver_id', cont.drivers.delete );
+
+	server.get( '/vehicles', cont.vehicles.index );
+	server.post( '/vehicles', cont.vehicles.create );
+	server.get( '/vehicles/:vehicle_id', cont.vehicles.view );
+	server.put( '/vehicles/:vehicle_id', cont.vehicles.update );
+
+	server.get( '/schedules', cont.schedules.index );
+	server.post( '/schedules', cont.schedules.create );
+	server.get( '/schedules/:schedule_id', cont.schedules.view );
+	server.put( '/schedules/:schedule_id', cont.schedules.update );
+};
